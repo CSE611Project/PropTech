2cf32e015aad5ac1db8dd24af4af68c1e8ffb765
r-zheng <raymond.zheng@stonybrook.edu>
2021-03-09 12:54:49 -0500

    stuff to make the server work. use: npm run start

--- src/server/router.js
+++ src/server/router.js
@@ -14,7 +14,7 @@ const userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 router.post('/signup', (request, response) => {
     const email = request.body.email;
     const password = request.body.password;
-    console.log(request.body);
+    
     userPool.signUp(email, password, [], null, (err, data) => {
         if(err)
             response.json(err);
