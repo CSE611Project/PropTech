fdbf978e86738b4fdc249941f11fac442da55c04
r-zheng <raymond.zheng@stonybrook.edu>
2021-03-10 22:47:24 -0500

    enable function added

--- src/server/router.js
+++ src/server/router.js
@@ -6,18 +6,17 @@ const cognito = new aws.CognitoIdentityServiceProvider({region:'us-east-2'});
 
 const config = require('./config.json');
 
-const poolData = {
-    UserPoolId: config.cognito.userPoolId,
-    ClientId: config.cognito.clientId
-};
-
 router.post('/signup', (request, response) => {
     const params = {
         ClientId: config.cognito.clientId,
         Password: request.body.password,
         Username: request.body.email
     }
-    registerUser(params)
+    registerUser(params);
+});
+
+router.post('/enable', (request, response) => {
+    enableUser(request.body.email);
 });
 
 const registerUser = async (params) => {
@@ -39,6 +38,23 @@ const disableUser = async (username) => {
         }
 
         cognito.adminDisableUser(params, (err, data) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(data);
+            }
+        })
+    })
+}
+
+const enableUser = async (username) => {
+    return await new Promise((resolve, reject) => {
+        const params = {
+            UserPoolId: config.cognito.userPoolId,
+            Username: username
+        }
+
+        cognito.adminEnableUser(params, (err, data) => {
             if(err) {
                 reject(err);
             } else {
