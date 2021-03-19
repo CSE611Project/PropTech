cdf8c539148edddf0c54d0b809c0020e8eca95ce
r-zheng <raymond.zheng@stonybrook.edu>
2021-03-19 03:09:43 -0400

    changes to authentication

--- client/src/components/LoginPage.js
+++ client/src/components/LoginPage.js
@@ -2,22 +2,78 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import "./../App.css";
 import Navigation from "./Navigation.js";
-import AdminAfterSign from "./AdminAfterSign.js"
+import AdminAfterSign from "./AdminAfterSign.js";
+import {cognito, userPool} from "./UserPool"
 
 class LoginPage extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      email: '',
+      password: ''
+    }
+    this.changeEmail = this.changeEmail.bind(this)
+    this.changePassword = this.changePassword.bind(this)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  changeEmail(event) {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  changePassword(event) {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
+  onSubmit(event) {
+    event.preventDefault()
+
+    const loginDetails = {
+      Username: this.state.email,
+      Password: this.state.password
+    }
+    const authenticationDetails = new cognito.AuthenticationDetails(loginDetails);
+    const userDetails = {
+      Username: this.state.email,
+      Pool: userPool
+    }
+    const cognitoUser = new cognito.CognitoUser(userDetails);
+    cognitoUser.authenticateUser(authenticationDetails, {
+      onSuccess: function (result) {
+        console.log(result); // TODO save result
+      },
+      onFailure: function(err) {
+        console.log(err);
+      }
+    })
+    aftersign()
+  }
+
   render() {
     return (
       <div>
         <Navigation />
         <div className="LoginPage">
           <header className="Login-header">
-            <h1 className="Title">PropTech</h1>
-            <label className="EmailLabel">Email</label>
-            <input type="text" />
-            <label className="PasswordLabel">Password</label>
-            <input type="password" />
-            <button className="button" onClick={aftersign}>Login</button>
+            <form onSubmit={this.onSubmit}>
+            <h1>Please enter the information below</h1>
+            <div id="login">
+            <label htmlFor="email">Email</label>
+              <input type="text" placeholder="Email"
+              onChange={this.changeEmail}
+              value={this.state.email} />
+            <label htmlFor="Password">Password</label>
+              <input type="password" placeholder="Password"
+              onChange={this.changePassword}
+              value={this.state.password} />
+            <button className="LoginButton" type="submit" value="submit">Login</button>
             <button className="reset">Reset Password</button>
+            </div>
+            </form>
           </header>
         </div>
       </div>
--- client/src/components/UserPool.js
+++ client/src/components/UserPool.js
@@ -1,8 +1,9 @@
-import { CognitoUserPool } from "amazon-cognito-identity-js";
+const cognito = require("amazon-cognito-identity-js");
 
 const poolData = {
   UserPoolId: "us-east-2_9sP4CUJwB",
   ClientId: "4lopeqiaq0bmrmvkccb09u6lq8"
 }
 
-export default new CognitoUserPool(poolData);
+const userPool = new cognito.CognitoUserPool(poolData);
+export {cognito, userPool}
