bd01e610e04298b3748b908d8e640f7d29d5329b
xinyuche <xinyuche@buffalo.edu>
2021-03-18 23:21:38 -0400

    fixed sql injection problem and added delete user_id from database function

--- server/src/database_router.js
+++ server/src/database_router.js
@@ -23,6 +23,42 @@ router.use(cors());
 //     });
 // })
 
+// The following commented code is for testing the prepared statement
+// router.post('/testInsert',(req,res)=>{
+//     const {user_id} = req.body;
+//     console.log(req.body);
+//     console.log(`user_id: ${user_id}`);
+//     let sql = `INSERT INTO user (user_id) VALUES (?)`;
+//     let inserts = [user_id];
+//     database.connection.query(sql,inserts, function(err){
+//         // check error type later
+//         if(err) {
+//             console.log('error found');
+//             res.status(500).send('Sorry, we cannot find that!');
+//         } else {
+//             console.log('ok');
+//             res.send('added');
+//         }
+//     });
+// })
+
+// The following commented code is for testing delete user
+// router.post('/testDelete',(req,res)=>{
+//     const {user_id} = req.body;
+//     console.log(`user_id: ${user_id}`);
+//     let sql = `DELETE FROM user WHERE ?? = ?`;
+//     let inserts = ["user_id", user_id];
+//     database.connection.query(sql,inserts, function(err){
+//         // check error type later
+//         if(err) {
+//             console.log('error found');
+//             res.status(500).send('Sorry, we cannot delete that!');
+//         } else {
+//             console.log('ok');
+//             res.send('deleted');
+//         }
+//     });
+// })
 module.exports = router;
 
 
