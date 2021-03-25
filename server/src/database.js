7596b37637e539baed2e8a80ef14b26d1b504f67
xinyuche <xinyuche@buffalo.edu>
2021-03-25 19:11:30 -0400

    finished update tenant info database method

--- server/src/database.js
+++ server/src/database.js
@@ -33,6 +33,7 @@ function insertUserIdToDatabase(user_id) {
 }
 
 // delete user_id from user table
+// should able to delete all data related to the user needs to be deleted
 function deleteUserIdFromDatabase(user_id){
     let sql = `DELETE FROM user WHERE ?? = ?`;
     let inserts = ["user_id", user_id];
@@ -45,7 +46,47 @@ function deleteUserIdFromDatabase(user_id){
         }
     });
 }
+
+// // return JSON contains all tenant info for a property
+// function getAllTenantInfo(property_id, user_id){
+//     let sql = `SELECT * FROM tenant WHERE ?? = ?`;
+//     let inserts = ["property_id", property_id];
+//
+//
+// }
+
+// update tenant info
+function updateTenantInfo(tenant_id, update_info){
+    const ready_to_update = JSON.parse(update_info);
+    let name = ready_to_update.name;
+    let email = ready_to_update.email;
+    let address = ready_to_update.address;
+    let building_share = ready_to_update.building_share;
+
+    let sql = `UPDATE tenant set ?? = ?, ?? = ?, ?? = ?, ?? = ? WHERE ?? = ?`;
+    let inserts = ["name", name, "email", email, "address", address, "building_share", building_share, "tenant_id", tenant_id];
+
+    connection.query(sql, inserts, function (err) {
+        if(err) {
+            console.log(`not able to update tenant_id: ${tenant_id} from database`);
+        } else {
+            console.log('updated');
+        }
+    })
+
+}
+
+// // add new tenant for a property
+// function addNewTenant(property_id, tenant_info){
+//
+// }
+
+// // delete tenant
+// function deleteTenant(property_id, tenant_id){
+//
+// }
 exports.establishDatabaseConnection = establishDatabaseConnection;
 exports.connection = connection;
 exports.insertUserIdToDatabase = insertUserIdToDatabase;
 exports.deleteUserIdFromDatabase = deleteUserIdFromDatabase;
+exports.updateTenantInfo = updateTenantInfo;
