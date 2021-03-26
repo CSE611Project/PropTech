08ebebfbf1a7154fd2fd4a12f9d28fa6551bfb72
r-zheng <raymond.zheng@stonybrook.edu>
2021-03-26 01:58:42 -0400

    resolved merge conflicts 1

--- client/src/components/UserPool.js
+++ client/src/components/UserPool.js
@@ -1,8 +1,4 @@
 const cognito = require("amazon-cognito-identity-js");
-<<<<<<< HEAD
-=======
-const AWS = require('aws-sdk');
->>>>>>> Front-end
 
 const poolData = {
   UserPoolId: "us-east-2_9sP4CUJwB",
@@ -10,21 +6,4 @@ const poolData = {
 }
 
 const userPool = new cognito.CognitoUserPool(poolData);
-<<<<<<< HEAD
-=======
-
-{/*const cognitoidentityserviceprovider = new AWS.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'});
-const params = {
-  "AttributesToGet": [],
-  "Filter": "",
-  "Limit": 10,
-  "UserPoolId": "us-east-2_9sP4CUJwB"
-}
-
-cognitoidentityserviceprovider.listUsers(params, function (err, data) {
-  if (err) console.log(err, err.stack); // an error occurred
-  else console.log(data);           // successful response
-});*/}
-
->>>>>>> Front-end
 export {cognito, userPool}
