c342afa4a01e8240f1cea9a442f195129cfa83b7
r-zheng <raymond.zheng@stonybrook.edu>
2021-04-15 00:15:07 -0400

    prepare for build

--- client/src/components/SideMenu.js
+++ client/src/components/SideMenu.js
@@ -1,37 +1,8 @@
-
 import React from "react";
 import { Component } from "react";
 import ReactDOM from "react-dom";
 import "./../App.css";
-import Cookies from "js-cookie";
-import TenantInfo from "./TenantInfo"
-import BillingHistory from "./BillingHistory"
-import UtilityBillMeter from "./UtilityBillMeter"
-import UtilityBillSubmeter from "./UtilityBillSubmeter"
-
-import { makeStyles } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Drawer from "@material-ui/core/Drawer";
-import Box from "@material-ui/core/Box";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemIcon from "@material-ui/core/ListItemIcon";
-import ListItemText from "@material-ui/core/ListItemText";
-import Typography from "@material-ui/core/Typography";
-import Divider from "@material-ui/core/Divider";
-import IconButton from "@material-ui/core/IconButton";
-import Badge from "@material-ui/core/Badge";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Paper from "@material-ui/core/Paper";
-import Link from "@material-ui/core/Link";
-import Button from "@material-ui/core/Button";
-import FormLabel from "@material-ui/core/FormLabel";
-import FormControl from "@material-ui/core/FormControl";
-import FormGroup from "@material-ui/core/FormGroup";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
+import GenerateInvoice from "./GenerateInvoice";
 import axios from "axios";
 
 class SideMenu extends Component {
@@ -81,7 +52,7 @@ function edit_profile() {
 }
 
 function manage_utility() {
-  const ele = <div></div>
+  const ele = <div></div>;
   return ReactDOM.render(ele, document.getElementById("root"));
 }
 
