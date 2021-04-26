275787c1c8e0d022c71eb30f8eb5334dc65528ce
r-zheng <raymond.zheng@stonybrook.edu>
2021-04-26 19:36:03 -0400

    should work on deploy as well now

--- server/src/index.js
+++ server/src/index.js
@@ -7,10 +7,10 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 // For local testing and development
-app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
+// app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
 
 // For build
-// app.use(cors({ credentials: true, origin: "http://3.131.169.6" }));
+app.use(cors({ credentials: true, origin: "http://3.131.169.6:3001" }));
 app.use(express.json());
 app.use(cookieParser());
 app.use("", routesUrls);
