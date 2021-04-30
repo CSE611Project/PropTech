b326d2db458f194a5fce2d507ef8eb4e5dc69223
r-zheng <raymond.zheng@stonybrook.edu>
2021-04-30 01:21:57 -0400

    update cors

--- server/src/index.js
+++ server/src/index.js
@@ -7,11 +7,12 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 
 // For local testing and development
-app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
+// app.use(cors({ credentials: true, origin: "http://localhost:3001" }));
 
 // For build
-// app.use(cors({ credentials: true, origin: "http://3.131.169.6:3001" }));
-app.use(express.json({limit: '50mb'}));;
+app.use(cors({ credentials: true, origin: "http://3.131.169.6" }));
+
+app.use(express.json({ limit: "50mb" }));
 app.use(cookieParser());
 app.use("", routesUrls);
 app.use("", databaseRouter);
